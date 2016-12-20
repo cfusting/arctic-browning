dcf5505a92bf2a5fdec55aea6255770f4d3a2678
Chris Fusting <cfusting@gmail.com>
2016-12-20 10:13:56 -0500

    Adjusted correct array to match only pefect pixels

--- scripts/lib_test.py
+++ scripts/lib_test.py
@@ -9,7 +9,7 @@ class MaskTest(unittest.TestCase):
     def test(self):
         inputarray = np.array([-3000, 3244, 1000, 2320, 1232, 9093, -3000])
         reliability = np.array([-1, 0, 1, 2, 3, 0, 0])
-        correctmask = np.array([1, 0, 0, 1, 1, 0, 1])
+        correctmask = np.array([1, 0, 1, 1, 1, 0, 1])
         build_qa_mask(inputarray, reliability)
         npt.assert_array_equal(reliability, correctmask)
-        self.assertEqual(ma.array(inputarray, mask=reliability).sum(), 13337)
+        self.assertEqual(ma.array(inputarray, mask=reliability).sum(), 12337)
