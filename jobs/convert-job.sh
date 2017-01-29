c0372226834ed9db2885578ea82b5a76954e8c01
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-01-29 10:42:18 -0500

    Adjusted job specifics

--- jobs/convert-job.sh
+++ jobs/convert-job.sh
@@ -2,11 +2,11 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=10:00:00
 #PBS -N modis-convert
-cd $HOME/ndvi_terra
-ids=$(ls | grep -oh A[0-9]*[0-9] | uniq)
+cd $HOME/v5_ndvi_terra
+ids=$(ls | grep -oh A[0-9]*[0-9] | sort | uniq)
 for i in $ids
 do
         ls | grep $i".*hdf$" > $i".filelist"
-	source $HOME/browning/scripts/convert.sh $i".filelist" 
+	source $HOME/arctic-browning/scripts/convert.sh $i".filelist" 
         rm $i".filelist"
 done
--- scripts/temporal_statistics.sh
+++ scripts/temporal_statistics.sh
@@ -3,6 +3,6 @@ source $HOME"/.bash_profile"
 source $HOME"/modis/bin/activate"
 SCRIPT=$HOME"/arctic-browning/scripts/temporal_statistics.py"
 NDVIOUT=$HOME"/results/ndvi_temporal_stats.csv"
-DIRECTORY=$HOME"/ndvi_terra/mosaics"
+DIRECTORY=$HOME"/v5_ndvi_terra/mosaics"
 echo "year,mean,sd,min,max" > $NDVIOUT
-python $SCRIPT -s 2000 -e 2016 -f 162 -l 255 -d $DIRECTORY -k 'clipped_mosaic.*NDVI' -r 'clipped_mosaic.*reliability' -j '\d{7}' >> $NDVIOUT
+python $SCRIPT -s 2000 -e 2016 -f 161 -l 232 -d $DIRECTORY -k 'clipped_mosaic.*NDVI' -r 'clipped_mosaic.*reliability' -j '\d{7}' -v -b $HOME/job_logs/temporal_stats.log >> $NDVIOUT
