96e5b0435cf64b751a5ea22bc50dbebc017b4c03
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-04-25 09:14:01 -0400

    Fixes

--- jobs/ml/batch_symbolic.sh
+++ jobs/ml/batch_symbolic.sh
@@ -2,7 +2,6 @@
 for i in `seq 1 $3`
 do
 	seed=$RANDOM
-	qsub -v data=$1,seed=$seed,name=$2 single_gp_runner.pbs
+	qsub -v data=$1,seed=$seed,name=$2 "$HOME/arctic-browning/jobs/ml/symbolic.sh"
 	echo "Run $i: $2 started with seed $seed"
-	sleep 1;
 done
