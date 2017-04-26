5a3ff7eb0173ff0ccba1a3103993a601ea4927cb
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-04-26 17:25:45 -0400

    fixes

--- jobs/ml/symbolic_validate.sh
+++ jobs/ml/symbolic_validate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=1,pmem=8gb,pvmem=8gb
+#PBS -l nodes=1:ppn=1,pmem=32gb,pvmem=32gb
 #PBS -l walltime=03:00:00
 #PBS -N symbolic_validate
 #PBS -o /users/c/f/cfusting/job_logs
