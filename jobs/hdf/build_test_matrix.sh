78fee7eea04018b13d2989d46e21280f909ac245
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-05-01 16:10:22 -0400

    fixes

--- jobs/hdf/build_test_matrix.sh
+++ jobs/hdf/build_test_matrix.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1,pmem=32gb,pvmem=32gb
 #PBS -l walltime=01:00:00
-#PBS -N dmatrix
+#PBS -N test_matrix
 #PBS -o /users/c/f/cfusting/job_logs
 #PBS -e /users/c/f/cfusting/job_logs
 #PBS -q shortq
+cd ~/modis_data/lst_8day_1km
+find `pwd` -name "*hdf" > hdfs.list
+cd ~/modis_data/ndvi_monthly_1km
+find `pwd` -name "*hdf" > hdfs.list
 source $HOME/arctic-browning/scripts/hdf/build_test_matrix.sh
--- jobs/hdf/build_training_matrix_job.sh
+++ jobs/hdf/build_training_matrix_job.sh
@@ -5,4 +5,8 @@
 #PBS -o /users/c/f/cfusting/job_logs
 #PBS -e /users/c/f/cfusting/job_logs
 #PBS -q shortq
+cd ~/modis_data/lst_8day_1km
+find `pwd` -name "*hdf" > hdfs.list
+cd ~/modis_data/ndvi_monthly_1km
+find `pwd` -name "*hdf" > hdfs.list
 source $HOME/arctic-browning/scripts/hdf/build_training_matrix.sh
