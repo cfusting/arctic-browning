d9e37a89b654f3ec58bf870702d2c25254c70577
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-05-01 14:29:31 -0400

    Added snow job

---
+++ jobs/acquisition/get_other.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=03:00:00
+#PBS -N get-data
+#PBS -q shortq
+#PBS -e /users/c/f/cfusting/job_logs
+#PBS -o /users/c/f/cfusting/job_logs
+modis_download.py -r -D 1 -U cfusting -P Blueferret1 -t $tile -p $dataset -f 2011-01-01 -e 2016-12-31 $directory
