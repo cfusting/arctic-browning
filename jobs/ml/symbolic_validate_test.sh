026a4fcea0d1cc10858f8fb62d2f1dd2dab27c76
Chris Fusting <cfusting@gmail.com>
2017-05-02 07:17:29 -0400

    Rolled testing into validation

--- jobs/ml/symbolic_validate_test.sh
+++ jobs/ml/symbolic_validate_test.sh
@@ -6,4 +6,4 @@
 #PBS -e /users/c/f/cfusting/job_logs
 #PBS -q shortq
 export PYTHONPATH=$HOME/gp_mecl:$HOME/arctic-browning
-python "$HOME/arctic-browning/scripts/ml/symbolic_validate.py" -t $training -n $name -r $results
+python "$HOME/arctic-browning/scripts/ml/symbolic_validate_test.py" -t $training -j $testing -n $name -r $results -s 100000
