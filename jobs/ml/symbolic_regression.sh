da634047a4090674d16fd55032f1346736567215
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-05-03 22:10:13 -0400

    setup job

--- jobs/ml/symbolic_regression.sh
+++ jobs/ml/symbolic_regression.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1,pmem=8gb,pvmem=8gb
-#PBS -l walltime=07:00:00
+#PBS -l walltime=30:00:00
 #PBS -N symbolic
 #PBS -o /users/c/f/cfusting/job_logs
 #PBS -e /users/c/f/cfusting/job_logs
