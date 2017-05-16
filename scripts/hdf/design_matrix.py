a865eabc7e86f724c4858fc32620ae879cafbb2a
Chris Fusting <cfusting@gmail.com>
2017-05-15 21:37:12 -0400

    Fixes

--- scripts/hdf/design_matrix.py
+++ scripts/hdf/design_matrix.py
@@ -106,6 +106,7 @@ def build_predictor_matrix(file_paths, first_year, last_year, t0, delta, eta, da
     logging.info("Percent missing data in columns: " + str(missing_percent))
     missing_values = missing_percent >= args.missing_ratio
     missing_indices = missing_values.nonzero()
+    logging.debug("Missing indices: " + str(missing_indices))
     if args.missing_ratio and missing_indices:
         logging.info("Deleting columns: " + str(missing_indices))
         cleaned_matrix = np.delete(masked_matrix, missing_indices)
