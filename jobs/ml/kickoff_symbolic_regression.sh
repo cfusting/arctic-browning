ef68a8e05b633e3d2ac9dd8263eb03c0c9799ad1
Chris Fusting <cfusting@gmail.com>
2017-05-16 14:29:04 -0400

    Minor changes to symbolic regression kickoff

--- jobs/ml/kickoff_symbolic_regression.sh
+++ jobs/ml/kickoff_symbolic_regression.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# usage training_data name runs
 for i in `seq 1 $3`
 do
 	seed=$RANDOM
-	qsub -v training=$1,seed=$seed,name=$2 "$HOME/arctic-browning/jobs/ml/symbolic_regression.sh"
+	qsub -v training=$1,seed=$seed,name=$2 "$ARCTIC_HOME/jobs/ml/symbolic_regression.sh"
 	echo "Run $i: $2 started with seed $seed"
 done
