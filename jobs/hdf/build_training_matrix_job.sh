93a4038140a47c086e6941c4bbeb0a1e6fab1c24
Chris Fusting <cfusting@gmail.com>
2017-05-17 15:02:56 -0400

    Deal with weird file creation issue

--- jobs/hdf/build_training_matrix_job.sh
+++ jobs/hdf/build_training_matrix_job.sh
@@ -7,9 +7,11 @@
 #PBS -q shortq
 source ~/.bash_profile
 uphdfs
+filename="training_matrix.hdf"
+rm $ARCTIC_DATA/$filename
 export PYTHONPATH=$HOME/gp_mecl:$ARCTIC_HOME
 python $ARCTIC_HOME"/scripts/hdf/design_matrix.py" \
 -l $MODIS_DATA_HOME"/lst_8day_1km/hdfs.list" \
 -n $MODIS_DATA_HOME"/ndvi_monthly_1km/hdfs.list" \
 -s $MODIS_DATA_HOME"/snow_8day_500m/hdfs.list" \
--y 2011 -j 2014 -t 255 -a 365 -e 0 -o $ARCTIC_DATA_HOME"/training_matrix.hdf" -v -m .3 -z .98
+-y 2011 -j 2014 -t 255 -a 365 -e 0 -o $ARCTIC_DATA_HOME/$filename -v -m .3 -z .98
