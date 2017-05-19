bc4dc071f9c2be715d8fda56858432146626e8ff
Chris Fusting <cfusting@gmail.com>
2017-05-18 20:19:53 -0400

    fixes

--- experiments/utils.py
+++ experiments/utils.py
@@ -11,10 +11,10 @@ def get_archive():
 
 def get_lst_and_snow_variable_names(lst_days, snow_days):
     lst_args = ["ARG" + str(x) for x in range(0, len(lst_days))]
-    lst_names = ["lst" + str(x) for x in lst_days]
+    lst_names = ["lst_" + str(x) for x in lst_days]
     first_args = dict(zip(lst_args, lst_names))
-    snow_args = ["ARG" + str(x) for x in range(len(lst_days), len(snow_days))]
-    snow_names = ["lst" + str(x) for x in lst_days]
+    snow_args = ["ARG" + str(x) for x in range(len(lst_days), len(lst_days) + len(snow_days))]
+    snow_names = ["snow_" + str(x) for x in lst_days]
     second_args = dict(zip(snow_args, snow_names))
     args = first_args.copy()
     args.update(second_args)
