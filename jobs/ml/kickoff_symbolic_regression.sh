114d6254cb454780352b3f15a878b40ac6643f48
Chris Fusting <cfusting@gmail.com>
2017-06-16 10:40:57 -0400

    Added data parameter

--- jobs/ml/kickoff_symbolic_regression.sh
+++ jobs/ml/kickoff_symbolic_regression.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# usage: source kickoff_symbolic_regression experiment_name num_runs
+# usage: source kickoff_symbolic_regression experiment_name num_runs data
 last=`pwd`
 cd $ARCTIC_RESULTS_HOME
 for i in `seq 1 $2`
 do
 	seed=$RANDOM
-	qsub -v training=$ARCTIC_DATA_HOME/training_matrix.hdf,seed=$seed,name=$1 "$ARCTIC_HOME/jobs/ml/symbolic_regression.sh"
+	qsub -v training=$4,seed=$seed,name=$1 "$ARCTIC_HOME/jobs/ml/symbolic_regression.sh"
 	echo "Run $i: $1 started with seed $seed"
 done
 cd $last
