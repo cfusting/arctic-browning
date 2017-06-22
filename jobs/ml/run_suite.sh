90706375f11ba20a17a3c8c3c64bac2891e79ab0
Chris Fusting <cfusting@gmail.com>
2017-06-22 12:07:52 -0400

    Udpated jobs

--- jobs/ml/run_suite.sh
+++ jobs/ml/run_suite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # runs data split
-experiments="control mandarin whistling_duck lesser_scaup bufflehead"
+experiments="control mandarin whistling_duck lesser_scaup"
 for experiment in ${experiments}
 do
   source ${ARCTIC_HOME}/jobs/ml/kickoff_symbolic_regression.sh ${experiment} ${1} ${2} ${3}
