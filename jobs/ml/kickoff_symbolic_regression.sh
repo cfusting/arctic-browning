14cf9b76144a43be42568df90af47b298033a98c
Chris Fusting <cfusting@gmail.com>
2017-06-22 08:39:49 -0400

    Better job names

--- jobs/ml/kickoff_symbolic_regression.sh
+++ jobs/ml/kickoff_symbolic_regression.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# usage: source kickoff_symbolic_regression experiment_name num_runs data split
+# usage: source kickoff_symbolic_regression training_data experiment_name num_runs data split
 last=`pwd`
 for i in `seq 1 ${2}`
 do
 	seed=${RANDOM}
-	qsub -v training=${3},seed=${seed},experiment=${1},split=${4} "${ARCTIC_HOME}/jobs/ml/symbolic_regression.sh"
+	qsub -N symbolic_${training}_${experiment}_${seed} -v training=${3},seed=${seed},experiment=${1},split=${4} "${ARCTIC_HOME}/jobs/ml/symbolic_regression.sh"
 	echo "Run ${i}: ${1} started with seed ${seed}"
 done
 cd ${last}
