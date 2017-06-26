9b2748d80196494e71dedb1a4aff482b1db7f48e
Chris Fusting <cfusting@gmail.com>
2017-06-26 17:13:07 -0400

    Updated scripts

--- jobs/results/evaluate_suite.sh
+++ jobs/results/evaluate_suite.sh
@@ -9,7 +9,7 @@ do
     trainset=${trainset} testset=${testset} datatype=${datatype} flags=${flags} experiment=${experiment} \
     ${ARCTIC_HOME}/jobs/results/evaluate.sh
   else
-     qsub -N evaluate_${1}_${experiment} \
+    qsub -N evaluate_${1}_${experiment} \
     -v experiment=${experiment},trainset=${1},testset=${2},datatype=${3},flags=${flags} \
     ${ARCTIC_HOME}/jobs/results/evaluate.sh
   fi
