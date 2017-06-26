a2718460c8981956c118e5b89c934b6b88d93f8c
Chris Fusting <cfusting@gmail.com>
2017-06-26 17:10:57 -0400

    Updated scripts

--- jobs/results/evaluate.sh
+++ jobs/results/evaluate.sh
@@ -9,6 +9,7 @@ source $HOME/.bash_profile
 export PYTHONPATH=$HOME/gp_mecl:$ARCTIC_HOME
 if [[ ${flags}  == *"v"* ]]
 then
+echo "Running symbolic validate"
 python ${ARCTIC_HOME}/scripts/results/symbolic_validate.py \
         -t ${ARCTIC_DATA_HOME}/${trainset}.${datatype} \
         -j ${ARCTIC_DATA_HOME}/${testset}.${datatype} \
@@ -17,6 +18,7 @@ python ${ARCTIC_HOME}/scripts/results/symbolic_validate.py \
 fi
 if [[ ${flags}  == *"f"* ]]
 then
+echo "Running frequent features"
 python ${ARCTIC_HOME}/scripts/plotting/frequent_features.py \
         -t ${ARCTIC_DATA_HOME}/${trainset}.${datatype} \
         -e ${experiment} \
@@ -24,6 +26,7 @@ python ${ARCTIC_HOME}/scripts/plotting/frequent_features.py \
 fi
 if [[ ${flags}  == *"c"* ]]
 then
+echo "Running change basis"
 python ${ARCTIC_HOME}/scripts/results/change_basis.py \
         -t ${ARCTIC_DATA_HOME}/${trainset}.${datatype} \
         -f ${ARCTIC_RESULTS_HOME}/${trainset}.${datatype}_${experiment}/features_${trainset}_${experiment}.txt \
@@ -37,6 +40,7 @@ python ${ARCTIC_HOME}/scripts/results/change_basis.py \
 fi
 if [[ ${flags}  == *"m"* ]]
 then
+echo "Running linear model"
 python ${ARCTIC_HOME}/scripts/ml/linear_model.py \
         -t ${ARCTIC_RESULTS_HOME}/${trainset}.${datatype}_${experiment}/optimal_basis_${trainset}.csv \
         -j ${ARCTIC_RESULTS_HOME}/${trainset}.${datatype}_${experiment}/optimal_basis_${testset}.csv \
