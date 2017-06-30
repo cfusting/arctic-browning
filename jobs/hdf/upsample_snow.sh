769f60cea82c110a0f89ed332d07739beefbaba5
Chris Fusting <cfusting@gmail.com>
2017-06-30 10:07:57 -0400

    Upsample snow udpate

--- jobs/hdf/upsample_snow.sh
+++ jobs/hdf/upsample_snow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/modis_data/snow_8day_500m
+cd ~/modis_data/snow_8day_500m_new
 for i in $(find $(pwd) -name "*hdf")
 do
   qsub -v filepath=$i ~/arctic-browning/jobs/hdf/snow_upsample.sh
