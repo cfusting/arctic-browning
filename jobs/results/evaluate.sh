c333dd70dda3c45b7043570dce16fcc2d37e6548
Christopher W Fusting <cfusting@bluemoon-user1.uvm.edu>
2017-07-04 09:32:21 -0400

    Tuning

--- jobs/results/evaluate.sh
+++ jobs/results/evaluate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1,pmem=32gb,pvmem=32gb
-#PBS -l walltime=03:00:00
+#PBS -l walltime=30:00:00
 #PBS -N symbolic_validate
 #PBS -o /users/c/f/cfusting/job_logs
 #PBS -e /users/c/f/cfusting/job_logs
-#PBS -q shortq
 source $HOME/.bash_profile
 export PYTHONPATH=$HOME/gp_mecl:$ARCTIC_HOME
 echo "Running evaluate with flags: ${flags}."
