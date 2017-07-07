a5e731a3774fda29d058d1d6c0c4f103ff71818c
Chris Fusting <cfusting@gmail.com>
2017-07-07 13:29:49 -0400

    Added experiment

--- jobs/ml/kickoff_symbolic_regression.sh
+++ jobs/ml/kickoff_symbolic_regression.sh
@@ -4,7 +4,8 @@ last=`pwd`
 for i in `seq 1 ${2}`
 do
 	seed=${RANDOM}
-	qsub -N symbolic_${3}_${1}_${seed} -v training=${3},seed=${seed},experiment=${1},split=${4} "${ARCTIC_HOME}/jobs/ml/symbolic_regression.sh"
+	qsub -N symbolic_${3}_${1}_${seed} -v training=${3},seed=${seed},experiment=${1},split=${4} \
+	"${ARCTIC_HOME}/jobs/ml/symbolic_regression.sh"
 	echo "Run ${i}: ${1} started with seed ${seed}"
 done
 cd ${last}
